7d702c6f2a257719300306b0e9920178adf4603f
erevear <erevear@gmail.com>
2015-04-15 12:14:05 -0700

    reseting password functionality works correctly

--- app/src/main/java/com/example/aidana/geotracker/CreateAccountActivity.java
+++ app/src/main/java/com/example/aidana/geotracker/CreateAccountActivity.java
@@ -29,7 +29,6 @@ public class CreateAccountActivity extends Activity {
     private String Question = "securityQuestion";
 
     SharedPreferences sharedpreferences;
-
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_account);
