c6e4a52be2d4a3657cbc05b074cdbf0034359552
myaidana <myaidana@gmail.com>
2015-05-04 14:24:05 -0700

    phase II in process

--- app/src/main/java/com/example/aidana/geotracker/NewPasswordActivity.java
+++ app/src/main/java/com/example/aidana/geotracker/NewPasswordActivity.java
@@ -1,56 +1,100 @@
+/*
+ * Eva Revear, Aidana Nurlanova, Zheng Young
+ */
 package com.example.aidana.geotracker;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.content.SharedPreferences.Editor;
+import android.widget.Toast;
+
+import com.example.aidana.geotracker.controller.GeoActivity;
+import com.example.aidana.geotracker.model.Account;
+import com.example.aidana.geotracker_user.R;
+
+import database.UserDB;
 
 /**
  * Created by erevear on 4/14/15.
+ * Edited by anurla92
  */
 public class NewPasswordActivity extends Activity {
 
 
     private Button setPassword;
     private EditText enterPassword;
+    private EditText enteruserName;
+    private Button back;
 
 
-    private String MyPREFERENCES = "myPrefs";
     private String Name = "nameKey";
     private String Email = "emailKey";
     private String Password = "passwordKey";
     private String Question = "securityQuestion";
 
-    SharedPreferences sharedpreferences;
 
 
+    UserDB userDB;
 
+    /**
+     * Method that creates instances
+     * @param savedInstanceState
+     */
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reset_password);
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
 
+        back=(Button)findViewById(R.id.resetpass_back);
         setPassword = (Button)findViewById(R.id.savenewpassword);
-        enterPassword = (EditText)findViewById(R.id.newpassword);
+        enterPassword = (EditText)findViewById(R.id.change_password);
+        enteruserName = (EditText)findViewById(R.id.changepass_username);
 
+        userDB = new UserDB(this);
 
         setPassword.setOnClickListener(new View.OnClickListener() {
-            Editor editor = sharedpreferences.edit();
+
+
+
+
 
             @Override
             public void onClick(View v) {
 
-                editor.putString(Password, enterPassword.getText().toString());
-                editor.commit();
+                String username = enteruserName.getText().toString();
+                String password = enterPassword.getText().toString();
 
+
+                //System.out.println(username);
+
+                if(username.length() > 0 && password.length() > 0) {
+                    userDB.updateContact(username, password);
+
+                    Intent i = new Intent(NewPasswordActivity.this, GeoActivity.class);
+                    startActivity(i);
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please fill in all fields";
+                    int duration = Toast.LENGTH_LONG;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
             }
         });
+        back.setOnClickListener(new View.OnClickListener() {
+            private Account newAccount;
 
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(NewPasswordActivity.this, SecurityQuestionActivity.class);
+                startActivity(i);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/aidana/geotracker/SecurityQuestionActivity.java
+++ app/src/main/java/com/example/aidana/geotracker/SecurityQuestionActivity.java
@@ -1,45 +1,63 @@
+/*
+ * Eva Revear, Aidana Nurlanova, Zheng Young
+ */
 package com.example.aidana.geotracker;
 
 import android.app.Activity;
-import android.content.Context;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.aidana.geotracker.controller.ForgotPasswordActivity;
+import com.example.aidana.geotracker.model.Account;
+import com.example.aidana.geotracker_user.R;
+
+import database.UserDB;
+
 /**
  * Created by erevear on 4/14/15.
+ * Edited by anurla
  */
 public class SecurityQuestionActivity extends Activity {
 
     private Button mySubmitButton;
     private TextView mySecurityQuestion;
-    private EditText mySecurityAnswer;
+    private Button back;
+    private EditText mySecurityAnswer; private String MyPREFERENCES = "myPrefs";
+    private String Question;
+    private String Name;
 
+    UserDB userDB;
+    private String answer;
 
-    private String MyPREFERENCES = "myPrefs";
-    private String Name = "nameKey";
-    private String Email = "emailKey";
-    private String Password = "passwordKey";
-    private String Question = "securityQuestion";
-
-    SharedPreferences sharedpreferences;
-
-
+    /**
+     * Method that creates instances
+     * @param savedInstanceState
+     */
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.security_question);
+        Intent intent = getIntent();
+        Question = intent.getStringExtra("Question");
+        Name = intent.getStringExtra("Name");
+
+        userDB = new UserDB(this);
 
         mySubmitButton = (Button)findViewById(R.id.submitanswer);
         mySecurityQuestion = (TextView)findViewById(R.id.question);
         mySecurityAnswer = (EditText)findViewById(R.id.answer);
+        back = (Button)findViewById(R.id.secquestion_back);
+        
 
-        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
 
-        mySecurityQuestion.setText(sharedpreferences.getString(Question, ""));
+        mySecurityQuestion.setText(Question);
+        System.out.println(Question);
         mySubmitButton.setOnClickListener(new View.OnClickListener() {
 
 
@@ -47,18 +65,49 @@ public class SecurityQuestionActivity extends Activity {
             public void onClick(View v) {
 
                 String inputAnswer = mySecurityAnswer.getText().toString();
-                String answer = sharedpreferences.getString(sharedpreferences.getString(Question, ""), "");
-                System.out.println(answer);
+                //String answer = sharedpreferences.getString(Question, "");
+                Cursor data = userDB.getData(Name);
+                data.moveToFirst();
+                String getName = data.getString(data.getColumnIndex(userDB.USER_COLUMN_NAME));
+                //question = data.getString(data.getColumnIndex(userDB.USER_COLUMN_SECURITY_Q));
+                answer = data.getString(data.getColumnIndex(userDB.USER_COLUMN_SECURITY_A));
+
+
+                //System.out.println(answer);
                 if(inputAnswer.equals(answer)) {
                     Intent i = new Intent(SecurityQuestionActivity.this, NewPasswordActivity.class);
                     startActivity(i);
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SecurityQuestionActivity.this);
+                    builder.setCancelable(true);
+                    builder.setTitle("Alert");
+                    builder.setMessage("Incorrect answer \nPlease, try again or register");
+                    builder.setInverseBackgroundForced(true);
+                    builder.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+
+                    AlertDialog alert = builder.create();
+                    alert.show();
                 }
 
             }
         });
 
+        back.setOnClickListener(new View.OnClickListener() {
+            private Account newAccount;
 
-
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SecurityQuestionActivity.this, ForgotPasswordActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 
